a07fedfdfe04bb5ce06ccb850a8efd45a1c32264
7art <sem85cv@yandex.ru>
2021-01-09 01:42:08 +0200

    added parcel and js modules

---
+++ src/model.js
@@ -0,0 +1,12 @@
+export const model = [
+    {type:'title', value:'Hello world!!!654654'},
+    {type:'text', value:'Here we go with some text'},
+    {type:'columns', value:[
+        '1111111111',
+        '2222222222',
+        '3333333333',
+        '4444444444'
+    ]},
+    {type:'image',value:'./assets/image.png'}
+
+]
